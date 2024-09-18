31dbcb4ad2db4295d1c7a458a792c1eca3141f0f
Paulius Preikša <preiksap@gmail.com>
2024-09-19 00:05:12 +0300

    wip: added example controllers

---
+++ backend/Controllers/HotelController.cs
@@ -0,0 +1,105 @@
+﻿using backend.Interfaces;
+using backend.RequestDtos.Hotel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[Route("hotel")]
+public class HotelController : ControllerBase
+{
+    private readonly IHotelService _hotelService;
+
+    public HotelController(IHotelService hotelService)
+    {
+        _hotelService = hotelService;
+    }
+
+    [HttpGet]
+    [Route("list")]
+    public async Task<IActionResult> GetAllHotels()
+    {
+        try
+        {
+            var hotels = await _hotelService.GetAllHotels();
+            return Ok(hotels);
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                _ => StatusCode(500, "Error occured while getting hotels")
+            };
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetHotelById([FromQuery] Guid id)
+    {
+        try
+        {
+            var hotel = _hotelService.GetHotelById(id);
+            return Ok(hotel);
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                "Hotel not found" => NotFound("Hotel not found"),
+                _ => StatusCode(500, "Error occured while getting hotel")
+            };
+        }
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddHotel([FromBody] AddHotelRequest request)
+    {
+        try
+        {
+            await _hotelService.AddHotel(request);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                _ => StatusCode(500, "Error occured while adding hotel")
+            };
+        }
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> UpdateHotel([FromBody] EditHotelRequest request)
+    {
+        try
+        {
+            await _hotelService.UpdateHotel(request);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                "Hotel not found" => NotFound("Hotel not found"),
+                _ => StatusCode(500, "Error occured while updating hotel")
+            };
+        }
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteHotel([FromQuery] Guid id)
+    {
+        try
+        {
+            await _hotelService.DeleteHotel(id);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                "Hotel not found" => NotFound("Hotel not found"),
+                _ => StatusCode(500, "Error occured while deleting hotel")
+            };
+        }
+    }
+}
---
+++ backend/Controllers/OrderController.cs
@@ -0,0 +1,128 @@
+﻿using backend.Interfaces;
+using backend.RequestDtos.Order;
+using backend.RequestDtos.Price;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[Route("order")]
+public class OrderController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+    private readonly ICalculationsService _calculationsService;
+
+    public OrderController(IOrderService orderService, ICalculationsService calculationsService)
+    {
+        _orderService = orderService;
+        _calculationsService = calculationsService;
+    }
+
+    [HttpGet]
+    [Route("list")]
+    public async Task<IActionResult> GetAllOrders()
+    {
+        try
+        {
+            var orders = await _orderService.GetAllOrders();
+            return Ok(orders);
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                _ => StatusCode(500, "Error occured while getting orders")
+            };
+        }
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetOrderById([FromQuery] Guid id)
+    {
+        try
+        {
+            var order = _orderService.GetOrderById(id);
+            return Ok(order);
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                "Order not found" => NotFound("Order not found"),
+                _ => StatusCode(500, "Error occured while getting order")
+            };
+        }
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddOrder([FromBody] AddOrderRequest request)
+    {
+        try
+        {
+            await _orderService.AddOrder(request);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                "Hotel not found" => NotFound("Hotel not found"),
+                _ => StatusCode(500, "Error occured while adding order")
+            };
+        }
+    }
+    
+    [HttpPatch]
+    public async Task<IActionResult> UpdateOrder([FromBody] EditOrderRequest request)
+    {
+        try
+        {
+            await _orderService.UpdateOrder(request);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                "Order not found" => NotFound("Order not found"),
+                "Hotel not found" => NotFound("Hotel not found"),
+                _ => StatusCode(500, "Error occured while updating order")
+            };
+        }
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> DeleteOrder([FromQuery] Guid id)
+    {
+        try
+        {
+            await _orderService.DeleteOrder(id);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                "Order not found" => NotFound("Order not found"),
+                _ => StatusCode(500, "Error occured while updating order")
+            };
+        }
+    }
+
+    [HttpPost]
+    [Route("price")]
+    public async Task<IActionResult> GetOrderPrice([FromBody] GetPriceRequest request)
+    {
+        try
+        {
+            var price = _calculationsService.CalculatePrice(request);
+            return Ok(price);
+        }
+        catch (Exception e)
+        {
+            return e.Message switch
+            {
+                _ => StatusCode(500, "Error occured while calculating order price")
+            };
+        }
+    }
+}
